9d1f9d91170838dd24dd87ce7facd92e9ed4a560
Vítor Almeida <vitor13almeida@gmail.com>
2026-07-13 12:44:55 +0100

    feat: create ResourcePage component with dynamic metadata generation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,19 @@
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between p-32 bg-white dark:bg-black sm:items-start">
-      <div className="flex flex-col gap-4 text-center">
+    <main className="flex w-full flex-col items-center p-32 gap-64">
+      <section className="flex flex-col gap-4 text-center">
         <h1 className="text-3xl-bold text-black">Data Explorer</h1>
         <p className="text-l-regular text-neutral-800">
           Data Viewer Tool (Next.js + TypeScript + Tailwind + ADS)
         </p>
-      </div>
+      </section>
+      <section className="flex flex-col gap-4 container">
+        <h2 className="text-2xl-bold text-black">Data exploration</h2>
+        <p className="text-m-regular text-neutral-800">
+          A tool that offers open data explorations to facilitate their
+          appropriation and reuse.
+        </p>
+      </section>
     </main>
   );
 }
