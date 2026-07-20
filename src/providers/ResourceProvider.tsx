d58b289a8d07e6e7792476e2e3512f66a0d1052a
Vítor Almeida <vitor13almeida@gmail.com>
2026-07-20 18:35:39 +0100

    feat: implement URL parameter handling for data fetching; refactor getData and add prepareUrlSearchParams utility

--- src/providers/ResourceProvider.tsx
+++ src/providers/ResourceProvider.tsx
@@ -11,6 +11,8 @@ import {
   ResourceDataResponse,
   ResourceStructureResponse,
 } from "@/services/types/Resources";
+import { prepareUrlSearchParams } from "@/utils/urlParams";
+import { useSearchParams } from "next/navigation";
 import {
   createContext,
   Dispatch,
@@ -47,8 +49,8 @@ export type ResourceContextType = {
 
   sortColumn: string | null;
   setSortColumn: Dispatch<string | null>;
-  sortDirection: string | null;
-  setSortDirection: Dispatch<string | null>;
+  sortDirection: "asc" | "desc" | null;
+  setSortDirection: Dispatch<"asc" | "desc" | null>;
 
   showFilters: boolean;
   setShowFilters: Dispatch<boolean>;
@@ -62,6 +64,10 @@ export const ResourceContext = createContext<ResourceContextType | undefined>(
 );
 
 export function ResourceProvider({ children }: { children: ReactNode }) {
+  const searchParams = useSearchParams();
+
+  const [isReady, setIsReady] = useState<boolean>(false);
+
   const [resourceId, setResourceId] = useState<string>("");
 
   const [data, setData] = useState<PaginatedDataResponse | null>(null);
@@ -76,7 +82,9 @@ export function ResourceProvider({ children }: { children: ReactNode }) {
   const [pageSize, setPageSize] = useState<number>(PAGE_SIZES[0]);
 
   const [sortColumn, setSortColumn] = useState<string | null>(null);
-  const [sortDirection, setSortDirection] = useState<string | null>(null);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc" | null>(
+    null,
+  );
 
   const [showFilters, setShowFilters] = useState<boolean>(false);
   const [filters, setFilters] = useState<Record<string, any>>({});
@@ -149,16 +157,90 @@ export function ResourceProvider({ children }: { children: ReactNode }) {
     });
   }, [startStructureTransition, resourceId]);
 
+  const setUrlParams = (
+    page: number = 0,
+    page_size: number = 20,
+    sortCol: string | null = null,
+    sortOrder: string | null = null,
+    filters: Record<string, any> | null = null,
+  ) => {
+    const params = prepareUrlSearchParams(
+      page,
+      page_size,
+      sortCol,
+      sortOrder,
+      filters,
+    );
+    window.history.replaceState(
+      null,
+      "",
+      `${window.location.pathname}?${params}`,
+    );
+  };
+
   const applyFilters = () => {
     appliedFilters.current = { ...filters };
+
+    void setUrlParams(
+      page,
+      pageSize,
+      sortColumn,
+      sortDirection,
+      appliedFilters.current,
+    );
     void loadData();
   };
 
   useEffect(() => {
-    if (!resourceId) return;
+    console.log("searchParams", searchParams.entries().toArray());
 
+    let filtersToSet: Record<string, string> = {};
+    searchParams
+      .entries()
+      .toArray()
+      .forEach((param) => {
+        const key = param[0];
+        const value = param[1];
+        switch (key) {
+          case "page":
+            setPage(value ? (Number(value) ?? 0) : 0);
+            break;
+          case "page_size":
+            setPageSize(value ? (Number(value) ?? 0) : 0);
+            break;
+          default:
+            if (key.endsWith("__sort")) {
+              setSortColumn(key.replace("__sort", ""));
+              setSortDirection(value === "desc" ? "desc" : "asc");
+            } else if (key.endsWith("__contains")) {
+              filtersToSet = {
+                ...filtersToSet,
+                [key.replace("__contains", "")]: value,
+              };
+            }
+            break;
+        }
+
+        console.log("filtersToSet", filtersToSet);
+
+        setFilters(filtersToSet);
+        appliedFilters.current = filtersToSet;
+      });
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    if (!resourceId && isReady) return;
+
+    void setUrlParams(
+      page,
+      pageSize,
+      sortColumn,
+      sortDirection,
+      appliedFilters.current,
+    );
     void loadData();
-  }, [resourceId, page, pageSize, sortColumn, sortDirection]);
+  }, [resourceId, page, pageSize, sortColumn, sortDirection, appliedFilters]);
 
   useEffect(() => {
     if (!resourceId) return;
