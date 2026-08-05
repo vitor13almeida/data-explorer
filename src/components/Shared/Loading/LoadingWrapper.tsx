9c171677c627df4d133f22e9cfdb2fbda1cc70d8
Vítor Almeida <vitor13almeida@gmail.com>
2026-08-05 11:40:54 +0100

    feat: refactor LoadingWrapper to use constant for loader size

--- src/components/Shared/Loading/LoadingWrapper.tsx
+++ src/components/Shared/Loading/LoadingWrapper.tsx
@@ -5,6 +5,8 @@ import { ReactNode } from "react";
 import { Loader } from "react-feather";
 import { useTranslation } from "react-i18next";
 
+const ICON_SIZE = 64;
+
 export type LoadingWrapperI = {
   children: ReactNode;
 };
@@ -20,7 +22,7 @@ export default function LoadingWrapper({ children }: LoadingWrapperI) {
 
       {isLoadingData && (
         <div className="absolute inset-0 z-10 flex flex-col items-center justify-center gap-12 rounded-lg bg-primary-100/50">
-          <Loader size={64} className="animate-spin text-primary-600" />
+          <Loader size={ICON_SIZE} className="animate-spin text-primary-600" />
           <span className="text-l-medium text-neutral-700">{t("loading")}</span>
         </div>
       )}
